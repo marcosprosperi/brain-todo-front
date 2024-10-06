b9610fcbebd12a82851cf5525d588fe9e9a88298
MarcosSpaceGuru <marcos.prosperi@space.guru>
2024-10-05 21:09:15 -0300

    add new date into utils

--- src/screens/tasks/utils/utils.ts
+++ src/screens/tasks/utils/utils.ts
@@ -1,21 +1,30 @@
 import { Task } from "@/Api/tasks/types/task";
 
-export type SortOption = "createdAsc" | "createdDesc" | "deadlineAsc" | "deadlineDesc"
+export type SortOption =
+  | "createdAsc"
+  | "createdDesc"
+  | "deadlineAsc"
+  | "deadlineDesc";
 
 export const sortTasks = (
   tasksToSort: Task[],
   sortOption: SortOption
 ): Task[] => {
   return [...tasksToSort].sort((a, b) => {
+    const createdAtA = new Date(a.createdAt);
+    const createdAtB = new Date(b.createdAt);
+    const deadlineA = new Date(a.deadline);
+    const deadlineB = new Date(b.deadline);
+
     switch (sortOption) {
       case "createdAsc":
-        return a.createdAt.getTime() - b.createdAt.getTime();
+        return createdAtA.getTime() - createdAtB.getTime();
       case "createdDesc":
-        return b.createdAt.getTime() - a.createdAt.getTime();
+        return createdAtB.getTime() - createdAtA.getTime();
       case "deadlineAsc":
-        return a.deadline.getTime() - b.deadline.getTime();
+        return deadlineA.getTime() - deadlineB.getTime();
       case "deadlineDesc":
-        return b.deadline.getTime() - a.deadline.getTime();
+        return deadlineB.getTime() - deadlineA.getTime();
       default:
         return 0;
     }
